a0a199f1a085fb3c0381f20b821cded11b655587
Robson Vaamonde <robsonvaamonde@gmail.com>
2020-11-17 20:18:27 -0300

    Add files via upload

---
+++ 01-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "Atualizando as lista do Apt"
+    #esse comando faz a atualização de todos os source.list do sistema
+    apt update
+
+echo -e "Atualizando os software"
+    apt -y upgrade
+
+echo -e "Forçando a atualização do sistema"
+    apt -y full-upgrade
+
+echo -e "Forçando a atualização de segurança"
+    apt -y dist-upgrade
+
+echo -e "Removendo software desnecessários"
+    apt -y autoremove
+    apt -y autoclean
+    apt clean
